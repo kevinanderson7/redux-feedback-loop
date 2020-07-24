89063b0d31fee3b0231865cd57fed0e2826f428f
Kevin Anderson <kmichael419@hotmail.com>
2020-07-24 16:17:32 -0500

    Understanding page is sending data over to store properly and moving to the next page

--- src/components/Pages/HowUnderstanding/HowUnderstanding.js
+++ src/components/Pages/HowUnderstanding/HowUnderstanding.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class HowUnderstanding extends Component {
+  state = {
+    understandingInput: '',
+  };
+
+  onInputChange = (input) => (event) => {
+    this.setState({
+      [input]: event.target.value,
+    });
+  };
+  onNextClick = (event) => {
+    //dispatch understandingInput to store
+    //go to next page
+    if (this.state.understandingInput === '') {
+      alert('Please enter a number');
+      return;
+    } else {
+      this.props.dispatch({
+        type: 'SET_UNDERSTANDING',
+        payload: this.state,
+      });
+      this.props.history.push('/support');
+    }
+  };
+
   render() {
     return (
       <div>
@@ -10,8 +34,12 @@ class HowUnderstanding extends Component {
           On a scale of 1-5, 5 being the best and 1 being the worst, how well do
           you feel like you understood today's content?
         </h3>
-        <input placeholder="Enter a number"></input>
-        <button>Next</button>
+        <input
+          type="number"
+          onChange={this.onInputChange('understandingInput')}
+          placeholder="Enter a number"
+        ></input>
+        <button onClick={this.onNextClick}>Next</button>
       </div>
     );
   }
