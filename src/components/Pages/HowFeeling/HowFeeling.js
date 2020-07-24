a52d9e1171746bb5612d4a5684d076b43baa7936
Kevin Anderson <kmichael419@hotmail.com>
2020-07-24 16:11:49 -0500

    Feeling page is sending data over to store properly and moving to the next page upon clicking next button

--- src/components/Pages/HowFeeling/HowFeeling.js
+++ src/components/Pages/HowFeeling/HowFeeling.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class HowFeeling extends Component {
+  state = {
+    feelingInput: '',
+  };
+
+  onInputChange = (input) => (event) => {
+    this.setState({
+      [input]: event.target.value,
+    });
+  };
+  onNextClick = (event) => {
+    //dispatch feelingInput to store
+    //go to next page
+    if (this.state.feelingInput === '') {
+      alert('Please enter a number');
+      return;
+    } else {
+      this.props.dispatch({
+        type: 'SET_FEELING',
+        payload: this.state,
+      });
+      this.props.history.push('/understanding');
+    }
+  };
+
   render() {
     return (
       <div>
@@ -10,8 +34,12 @@ class HowFeeling extends Component {
           On a scale of 1-5, 5 being the best and 1 being the worst, how do you
           feel about today's content?
         </h3>
-        <input placeholder="Enter a number"></input>
-        <button>Next</button>
+        <input
+          type="number"
+          onChange={this.onInputChange('feelingInput')}
+          placeholder="Enter a number"
+        ></input>
+        <button onClick={this.onNextClick}>Next</button>
       </div>
     );
   }
