1b7c3147b8b153229be55d4314efd2028941b0d9
Kevin Anderson <kmichael419@hotmail.com>
2020-07-26 09:35:54 -0500

    Leave new feedback button on success page now takes the user back to the beginning of the feedback app as it should

--- src/components/Pages/SuccessPage/SuccessPage.js
+++ src/components/Pages/SuccessPage/SuccessPage.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SuccessPage extends Component {
+  onNextClick = () => {
+    this.props.history.push('/');
+  };
+
   render() {
     return (
       <div>
